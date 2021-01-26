8bf715c6e44d32ea16b65a7689dbb2f24999b151
LukaP-BB <luka.pavageau@gmail.com>
2021-01-26 23:27:40 +0100

    pouet

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 intents.members = True
-bot = commands.Bot(command_prefix = '!', intents=intents) #création d'une instance de bot
+bot = commands.Bot(command_prefix = '$', intents=intents) #création d'une instance de bot
 
 DATE_HEURE_CONNEXION = datetime.datetime.now()
 
@@ -791,7 +791,7 @@ async def on_ready():
 def main():
     with open('token.txt', 'r') as token :
         t = token.read()
-        t = "NjU1NzIzMzk0MDAzNjMyMTI5.XfYP_w.SqH0-3I6CxoKPDlZABwY_Luyzqg"
+        # t = "NjU1NzIzMzk0MDAzNjMyMTI5.XfYP_w.SqH0-3I6CxoKPDlZABwY_Luyzqg"
         bot.run(t)
 
 if __name__ == '__main__':
